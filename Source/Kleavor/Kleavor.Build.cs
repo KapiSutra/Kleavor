15b86e1b06c1611cf4cf6e952a776dd2cce56075
tarnishablec <tarnishablec@outlook.com>
2025-04-26 15:55:49 +0800

    Init Kleavor

---
+++ Source/Kleavor/Kleavor.Build.cs
@@ -0,0 +1,61 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Kleavor : ModuleRules
+{
+	public Kleavor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicIncludePaths.AddRange(
+			new string[]
+			{
+				// ... add public include paths required here ...
+			}
+		);
+
+
+		PrivateIncludePaths.AddRange(
+			new string[]
+			{
+				// ... add other private include paths required here ...
+			}
+		);
+
+
+		PublicDependencyModuleNames.AddRange(
+			new[]
+			{
+				"Core",
+				// ... add other public dependencies that you statically link with here ...
+
+				"Combee",
+				"Scizor",
+				"Gengar",
+				"GameplayAbilities",
+				"EnhancedInput"
+			}
+		);
+
+
+		PrivateDependencyModuleNames.AddRange(
+			new[]
+			{
+				"CoreUObject",
+				"Engine",
+				"Slate",
+				"SlateCore",
+				// ... add private dependencies that you statically link with here ...	
+			}
+		);
+
+
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+				// ... add any modules that your module loads dynamically here ...
+			}
+		);
+	}
+}
